9e7434f2c61712f5ded36478b029840f14a62108
Anon <24657656+Anon20198@users.noreply.github.com>
2018-03-09 11:16:41 +0100

    Add files via upload

---
+++ src/AnsiColor.java
@@ -0,0 +1,37 @@
+public class AnsiColor {
+
+    // Reset String
+  private static final String ANSI_RESET          = "\u001B[0m";
+
+  public static final String	HIGH_INTENSITY      = "\u001B[1m";
+  public static final String	LOW_INTENSITY       = "\u001B[2m";
+
+  // String Style
+  public static final String  ITALIC              = "\u001B[3m";
+  public static final String	UNDERLINE           = "\u001B[4m";
+  public static final String	BLINK               = "\u001B[5m";
+  public static final String	RAPID_BLINK         = "\u001B[6m";
+  public static final String	REVERSE_VIDEO       = "\u001B[7m";
+  public static final String	INVISIBLE_TEXT      = "\u001B[8m";
+
+  // String Color
+  private static final String BLACK               = "\u001B[30m";
+  private static final String RED                 = "\u001B[31m";
+  private static final String GREEN               = "\u001B[32m";
+  private static final String YELLOW              = "\u001B[33m";
+  private static final String BLUE                = "\u001B[34m";
+  private static final String PURPLE              = "\u001B[35m";
+  private static final String CYAN                = "\u001B[36m";
+  private static final String WHITE               = "\u001B[37m";
+
+  // Background Color
+  private static final String BLACK_BACKGROUND    = "\u001B[40m";
+  private static final String RED_BACKGROUND      = "\u001B[41m";
+  private static final String GREEN_BACKGROUND    = "\u001B[42m";
+  private static final String YELLOW_BACKGROUND   = "\u001B[43m";
+  private static final String BLUE_BACKGROUND     = "\u001B[44m";
+  private static final String PURPLE_BACKGROUND   = "\u001B[45m";
+  private static final String CYAN_BACKGROUND     = "\u001B[46m";
+  private static final String WHITE_BACKGROUND    = "\u001B[47m";
+  
+}
